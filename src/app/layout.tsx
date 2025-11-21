79179c3c75fb3d9d15e92fb62f7eb7364396bc90
rayray39 <raynerlimfy@gmail.com>
2025-11-21 10:59:07 +0800

    Add shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${JetBrainsMono.className} antialiased`}
+        className={`${JetBrainsMono.className} dark antialiased`}
       >
         {children}
       </body>
