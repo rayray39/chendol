99aace70c7c842202de351e0bef3a26859222ae1
rayray39 <raynerlimfy@gmail.com>
2025-11-21 10:59:35 +0800

    Test Button functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+'use client'
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
-  return (
-    <div className="flex flex-col justify-center items-center h-screen">
-        <div className="text-3xl">Chendol-AI</div>
-        <div>Hello World</div>
-    </div>
-  );
+
+    const buttonHandler = () => {
+        console.log('button  clicked');
+    }
+
+    return (
+        <div className="flex flex-col justify-center items-center h-screen">
+            <div className="text-3xl">Chendol-AI</div>
+            <div>Hello World</div>
+
+            <Button onClick={buttonHandler}>click me</Button>
+        </div>
+    );
 }
