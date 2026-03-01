936e22a14ad6ee1ba49c577197a48e504eb62ae8
rayray39 <raynerlimfy@gmail.com>
2026-03-01 22:27:17 +0800

    Add client side validation for sign in

--- src/signIn/page.tsx
+++ src/signIn/page.tsx
@@ -1,5 +1,5 @@
 import { Input } from "@/components/ui/input"
-import { Field, FieldGroup, FieldLabel } from "@/components/ui/field"
+import { Field, FieldDescription, FieldGroup, FieldLabel } from "@/components/ui/field"
 import { Button } from "@/components/ui/button"
 import { SetStateAction, useState } from "react"
 
@@ -7,31 +7,53 @@ import { SetStateAction, useState } from "react"
 export default function SignIn() {
     const [isEmailEntered, setIsEmailEntered] = useState<boolean>(false);
     const [email, setEmail] = useState<string>('');
+    const [otp, setOtp] = useState<string>('');
+
+    const [isEmailEmptyOnSubmit, setIsEmailEmptyOnSubmit] = useState<boolean>(false);
+    const [isOtpEmptyOnSubmit, setIsOtpEmptyOnSubmit] = useState<boolean>(false);
 
     const handleEmailChange = (event: { target: { value: SetStateAction<string>; }; }) => {
         // handle email input change
         setEmail(event.target.value);
         if (event.target.value.length > 0) {
             setIsEmailEntered(true);
+            setIsEmailEmptyOnSubmit(false);
         } else {
             setIsEmailEntered(false);
         }
     }
 
+    const handleOtpChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+        // handle otp input change
+        setOtp(event.target.value);
+        if (event.target.value.length > 0) {
+            setIsOtpEmptyOnSubmit(false);
+        }
+    }
+
     const handleGetOTP = () => {
         // handle get otp button clicked
+        // TODO: include validation logic
         console.log('otp button clicked, sending otp to email...');
     }
 
     const handleSubmit = () => {
         // handle submit button clicked
+        if (!email) {
+            setIsEmailEmptyOnSubmit(true);
+            setIsEmailEntered(false);
+        }
+        if (!otp) {
+            setIsOtpEmptyOnSubmit(true);
+        }
         console.log(`entered email: ${email}`)
-        setEmail('');
+        // setEmail('');
+        // setOtp('');
     }
 
     return (
         <FieldGroup>
-            <Field>
+            <Field data-invalid={isEmailEmptyOnSubmit}>
                 <FieldLabel htmlFor="sign-in-email">Email</FieldLabel>
                 <Input 
                     id="sign-in-email" 
@@ -39,12 +61,34 @@ export default function SignIn() {
                     placeholder="johndoe@example.com" 
                     value={email}
                     onChange={handleEmailChange}
+                    aria-invalid={isEmailEmptyOnSubmit}
                 />
+                {
+                    isEmailEmptyOnSubmit ? 
+                    <FieldDescription>
+                        Email cannot be empty.
+                    </FieldDescription> :
+                    null
+                }
             </Field>
 
-            <Field>
+            <Field data-invalid={isOtpEmptyOnSubmit}>
                 <FieldLabel htmlFor="sign-in-otp">One-Time-Password</FieldLabel>
-                <Input id="sign-in-otp" type="password" placeholder="Enter OTP" />
+                <Input
+                    id="sign-in-otp" 
+                    type="password" 
+                    placeholder="Enter OTP" 
+                    value={otp}
+                    onChange={handleOtpChange}
+                    aria-invalid={isOtpEmptyOnSubmit}
+                />
+                {
+                    isOtpEmptyOnSubmit ? 
+                    <FieldDescription>
+                        OTP cannot be empty.
+                    </FieldDescription> :
+                    null
+                }
             </Field>
 
             <Field orientation="horizontal" className="justify-center">
