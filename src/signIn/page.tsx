b2653dd3f23d82bd05f7f8ae688dea6ee1f71da9
rayray39 <raynerlimfy@gmail.com>
2026-03-01 21:59:40 +0800

    Add onClick, onChange handlers

--- src/signIn/page.tsx
+++ src/signIn/page.tsx
@@ -1,14 +1,45 @@
 import { Input } from "@/components/ui/input"
 import { Field, FieldGroup, FieldLabel } from "@/components/ui/field"
 import { Button } from "@/components/ui/button"
+import { SetStateAction, useState } from "react"
 
 
 export default function SignIn() {
+    const [isEmailEntered, setIsEmailEntered] = useState<boolean>(false);
+    const [email, setEmail] = useState<string>('');
+
+    const handleEmailChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+        // handle email input change
+        setEmail(event.target.value);
+        if (event.target.value.length > 0) {
+            setIsEmailEntered(true);
+        } else {
+            setIsEmailEntered(false);
+        }
+    }
+
+    const handleGetOTP = () => {
+        // handle get otp button clicked
+        console.log('otp button clicked, sending otp to email...');
+    }
+
+    const handleSubmit = () => {
+        // handle submit button clicked
+        console.log(`entered email: ${email}`)
+        setEmail('');
+    }
+
     return (
         <FieldGroup>
             <Field>
                 <FieldLabel htmlFor="sign-in-email">Email</FieldLabel>
-                <Input id="sign-in-email" type="text" placeholder="johndoe@example.com" />
+                <Input 
+                    id="sign-in-email" 
+                    type="text" 
+                    placeholder="johndoe@example.com" 
+                    value={email}
+                    onChange={handleEmailChange}
+                />
             </Field>
 
             <Field>
@@ -17,8 +48,8 @@ export default function SignIn() {
             </Field>
 
             <Field orientation="horizontal" className="justify-center">
-                <Button className="sign-in-button" variant="outline">Get OTP</Button>
-                <Button className="sign-in-button" type="submit">Submit</Button>
+                <Button disabled={!isEmailEntered} className="sign-in-button" variant="outline" onClick={handleGetOTP}>Get OTP</Button>
+                <Button className="sign-in-button" type="submit" onClick={handleSubmit}>Submit</Button>
             </Field>
         </FieldGroup>
     )
