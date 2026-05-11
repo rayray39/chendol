a17b0b70656f84206acfcbdcdcd1bf5e76b1c5c3
rayray39 <raynerlimfy@gmail.com>
2026-05-11 22:09:29 +0800

    Add basic email checks

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -19,7 +19,32 @@ export default function SignIn() {
     const [isEmailEmptyOnSubmit, setIsEmailEmptyOnSubmit] = useState<boolean>(false);
     const [isOtpEmptyOnSubmit, setIsOtpEmptyOnSubmit] = useState<boolean>(false);
 
+    // checks whether email is valid
+    const [isEmailValid, setIsEmailValid] = useState<boolean>(true);
+
     const router = useRouter();
+
+    const suspiciousEmail = (email:string) => {
+        // dragonhunter@gmail.com
+        const trimmed_email = email.trim()  // remove any trailing and leading whitespaces
+        if (trimmed_email.split(" ").length > 1) {
+            // eg. 'dragonhunter@gmail.com random_text other_random_text' 
+            return true;
+        }
+        if (!trimmed_email.includes("@")) {
+            return true;
+        }
+        // perform checks on username
+        const userEmailUsername = trimmed_email.split("@")[0]   // dragonhunter
+        // perform checks on domain
+        const userEmailDomain = trimmed_email.split("@")[1]     // gmail.com
+        const commonEmailDomains = ['gmail.com', 'yahoo.com', 'hotmail.com', 'outlook.com']
+        if (!commonEmailDomains.includes(userEmailDomain)) {
+            return true
+        }
+
+        return false
+    }
 
     const handleEmailChange = (event: { target: { value: SetStateAction<string>; }; }) => {
         // handle email input change
@@ -30,6 +55,7 @@ export default function SignIn() {
         } else {
             setIsEmailEntered(false);
         }
+        setIsEmailValid(true)
     }
 
     const handleOtpChange = (event: { target: { value: SetStateAction<string>; }; }) => {
@@ -63,6 +89,10 @@ export default function SignIn() {
             return;
         }
         console.log(`entered email: ${email}`);
+        if (suspiciousEmail(email)) {
+            setIsEmailValid(false)
+            return;
+        }
         // validate OTP before navigation
         router.push('/home')
         // setEmail('');
@@ -71,7 +101,7 @@ export default function SignIn() {
 
     return (
         <FieldGroup>
-            <Field data-invalid={isEmailEmptyOnSubmit}>
+            <Field data-invalid={isEmailEmptyOnSubmit || !isEmailValid}>
                 <FieldLabel htmlFor="sign-in-email">Email</FieldLabel>
                 <Input 
                     id="sign-in-email" 
@@ -79,14 +109,15 @@ export default function SignIn() {
                     placeholder="johndoe@example.com" 
                     value={email}
                     onChange={handleEmailChange}
-                    aria-invalid={isEmailEmptyOnSubmit}
+                    aria-invalid={isEmailEmptyOnSubmit || !isEmailValid}
                 />
                 {
                     isEmailEmptyOnSubmit ? 
                     <FieldDescription>
                         Email cannot be empty.
-                    </FieldDescription> :
-                    null
+                    </FieldDescription> : (
+                        isEmailValid ? null : <FieldDescription>Please enter a valid email.</FieldDescription>
+                    )
                 }
             </Field>
 
